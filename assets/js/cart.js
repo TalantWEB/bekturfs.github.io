8bf21cecfa5bb4268c9a821504a58c9b57c501ec
bekturfs <bekturfs@gmail.com>
2020-06-08 16:15:49 +0600

    Converted es6 to es5

--- assets/js/cart.js
+++ assets/js/cart.js
@@ -1,24 +1,22 @@
 (function ($) {
   "use strict";
 
-  let products = {};
+  var products = {};
 
-  const setProducts = (prod, dataId) => {
-    products = {...products, ...prod};
-    updateProducts();
-    renderTotalPriceOfProducts();
-    reRenderTotalPriceOfOneProduct(dataId);
+  var setProducts = function(prod, dataId){
+    products = Object.assign(products, prod);
+    updateCartInSessionStorage();
   };
 
   // This function called when user open any page
-  const initCart = () => {
+  var initCart = function(){
 
     // Загружаем существующие товары из sessionStorage
     loadProducts();
 
     // Если есть элемент с таким классом, вещаем обработчик событий на кнопку "В КОРЗИНУ"
     if ($(".to-cart-btn")[0]){
-      $(".to-cart-btn").on('click', onClickToCartBtn);
+      $(".to-cart-btn").on("click", onClickToCartBtn);
     }
 
     // вызываем функцию для вывода числа выбранных товаров и общую сумму в шапке
@@ -28,41 +26,45 @@
     if ($(".cart-container")[0]){
       renderProducts();
     }
+
+    updateCartBtnListener();
   };
 
   // Function for loading selected products from sessionStorage
-  const loadProducts = () => {
+  var loadProducts = function(){
     if (sessionStorage.getItem("cart")) {
       products = JSON.parse(sessionStorage.getItem("cart"));
     }
 
-    for (let key in products){
-      let selector = `[data-id=${key.toString()}]`;
+    for (var key in products){
+      var selector = '[data-id=' + key.toString() + ']';
       $(selector).find(".to-cart-btn").html("Добавлено");
     }
   };
 
-  const onClickToCartBtn = (event) => {
-    let element = event.target;
-    let productElement = $(element).closest(".product-item");
+  var onClickToCartBtn = function(event){
+    var element = event.target;
+    var productElement = $(element).closest(".product-item");
     productElement = $(productElement);
 
-    let dataId = productElement.attr("data-id");
-    let img = productElement.find(".image img").attr("src");
-    let name = productElement.find(".title a").html();
-    let price = productElement.find(".inner-price").html();
+    var dataId = productElement.attr("data-id");
+    var img = productElement.find(".image img").attr("src");
+    var name = productElement.find(".title a").html();
+    var price = productElement.find(".inner-price").html();
+    var size = productElement.find(".size .size-inner").first().html();
+    var color = productElement.find(".color .color-inner").first().css("background-color");
 
-    let prod = { [dataId]: {name, img, price, amount: 1 } };
+    var prod = { [dataId]: {name, img, price, amount: 1, size, color} };
 
     addProductToCart(prod, dataId, productElement)
   };
 
   // Add or delete products in cart
-  const addProductToCart = (prod, dataId, productsElement) => {
+  var addProductToCart = function(prod, dataId, productsElement){
 
     // Проверяем не существует ли товар в списке товаров
     if (!products[dataId]){
-      products = {...prod, ...products};
+      products = Object.assign(products, prod);
       productsElement.find(".to-cart-btn").html("Добавлено");
     } else {
       delete products[dataId];
@@ -73,58 +75,53 @@
     renderHeaderMiniCart()
   };
 
-  const updateProducts = () => {
+  var updateCartInSessionStorage = function(){
     sessionStorage.setItem("cart", JSON.stringify(products));
     renderHeaderMiniCart();
   };
 
 
   // Function for calculating total price of selected products
-  const getTotalPriceOfProducts = () => {
-    let totalPrice = 0;
+  var getTotalPriceOfProducts = function(){
+    var totalPrice = 0;
 
-    for (let key in products){
+    for (var key in products){
       totalPrice += (parseFloat(products[key].price) * parseFloat(products[key].amount))
     }
 
     return totalPrice;
   };
 
-  const getTotalPriceOfOneProduct = (id) => {
+  var getTotalPriceOfOneProduct = function(id){
     return products[id] ? parseFloat(products[id].amount) * parseFloat(products[id].price) : 0
   };
 
-  const renderHeaderMiniCart = () => {
-    let numberOfProducts = 0;
-    for(let key in products){
+  var renderHeaderMiniCart = function(){
+    var numberOfProducts = 0;
+    for(var key in products){
       numberOfProducts += products[key]['amount'];
     }
-    $(".header-mini-cart-amount").html(`${numberOfProducts} ($${getTotalPriceOfProducts()})`);
+    $(".header-mini-cart-amount").html(numberOfProducts + '($' + getTotalPriceOfProducts() + ')');
   };
 
 
   // CART PAGE
   // Function for rendering products to cart.html
-  const renderProducts = () => {
+  var renderProducts = function(){
 
-    let out = "";
+    var out = "";
 
-    for (let key in products){
+    for (var key in products){
 
-      out += `
-      <tr class="cart-product" data-id="${key}">
-        <td class="pro-thumbnail"><a href="#"><img src="${products[key].img}" alt="" /></a></td>
-        <td class="pro-title"><a href="#">${products[key].name}</a></td>
-        <td class="pro-price"><span class="amount">$<span>${products[key].price}</span></span></td>
-        <td class="pro-quantity">
-            <div class="pro-qty">
-                <input type="text" value="${products[key].amount}">
-            </div>
-        </td>
-        <td class="pro-subtotal">$<span class="product-subtotal">${getTotalPriceOfOneProduct(key)}</span></td>
-        <td class="pro-remove"><a href="#">×</a></td>
-      </tr>
-      `
+      out += '<tr class="cart-product" data-id="' + key + '">';
+        out += '<td class="pro-thumbnail"><a href="#"><img src="' + products[key].img + '" alt="" /></a></td>';
+        out += '<td class="pro-title"><a href="#">' + products[key].name + '</a></td>';
+        out += '<td class="pro-price"><span class="amount">$<span>' + products[key].price + '</span></span></td>';
+        out += '<td class="pro-quantity"><div class="pro-qty"><input type="text" value="' + products[key].amount + '"></div></td>';
+        out += '<td class="pro-subtotal">$<span class="product-subtotal">' + getTotalPriceOfOneProduct(key) + '</span></td>';
+        out += '<td class="pro-remove"><a href="#">×</a></td>';
+      out += '</tr>'
+
     }
 
     $(".cart-container").html(out);
@@ -135,29 +132,28 @@
   };
 
   // Function for showing total price of selected products
-  const renderTotalPriceOfProducts = () => {
+  var renderTotalPriceOfProducts = function(){
     $(".total-amount").html(getTotalPriceOfProducts() + "$");
   };
 
-  const reRenderTotalPriceOfOneProduct = (id) => {
-    $(`[data-id="${id}"]`).find('.product-subtotal').html(getTotalPriceOfOneProduct(id));
-    // $(".cart-product").attr(id).html(getTotalPriceOfOneProduct(id))
+  var reRenderTotalPriceOfOneProduct = function(id){
+    $('[data-id="' + id + '"]').find('.product-subtotal').html(getTotalPriceOfOneProduct(id));
   };
 
-  const onDeleteListener = () => {
+  var onDeleteListener = function(){
     $('.pro-remove a').on('click', function (e) {
       e.preventDefault();
 
-      let dataId = $(this).closest('.cart-product').attr('data-id');
+      var dataId = $(this).closest('.cart-product').attr('data-id');
       if (dataId){
         delete products[dataId];
-        updateProducts();
+        updateCartInSessionStorage();
         renderProducts();
       }
     })
   };
 
-  const onQuantityChangeListeners = () => {
+  var onQuantityChangeListeners = function(){
     /*-----
       Quantity
   --------------------------------*/
@@ -167,11 +163,11 @@
     // Сработает когда пользователь изменяет количество
     $('.qtybtn').on('click', function()  {
 
-      let $button = $(this);
-      let dataId = $button.closest('.cart-product').attr('data-id');
+      var $button = $(this);
+      var dataId = $button.closest('.cart-product').attr('data-id');
 
-      let oldValue = $button.parent().find('input').val();
-      let newVal = 1;
+      var oldValue = $button.parent().find('input').val();
+      var newVal = 1;
       if ($button.hasClass('inc')) {
         newVal = parseFloat(oldValue) + 1;
       } else {
@@ -179,28 +175,44 @@
         newVal = oldValue > 1 ? (parseFloat(oldValue) - 1) : 1
       }
 
-      let prod = { [dataId]: { name: products[dataId].name, img: products[dataId].img ,price: products[dataId].price , amount: newVal }};
+      var prod = { [dataId]: { name: products[dataId].name, img: products[dataId].img ,price: products[dataId].price , amount: newVal }};
 
       $button.parent().find('input').val(newVal);
       setProducts(prod, dataId);
+      renderTotalPriceOfProducts();
+      reRenderTotalPriceOfOneProduct(dataId);
     });
 
     //
     $('.pro-qty input').on('input', function () {
 
-      let $input = $(this);
-      let dataId = $input.closest('.cart-product').attr('data-id');
+      var $input = $(this);
+      var dataId = $input.closest('.cart-product').attr('data-id');
 
-      let newVal = parseFloat($input.val());
+      var newVal = parseFloat($input.val());
       if (isNaN(newVal)){
         newVal = 0;
       }
 
-      let prod = { [dataId]: { name: products[dataId].name, img: products[dataId].img ,price: products[dataId].price , amount: newVal }};
+      var  prod = { [dataId]: { name: products[dataId].name, img: products[dataId].img ,price: products[dataId].price , amount: newVal }};
 
       setProducts(prod, dataId);
     })
   };
+
+  var updateCartBtnListener = function(){
+    $('.cart-buttons input').on('click', function (event) {
+      event.preventDefault();
+
+      for (var key in products){
+        products[key]['amount'] = 1;
+      }
+
+      updateCartInSessionStorage();
+      renderProducts();
+    })
+  };
+
 
   initCart();
 
--- assets/js/wishlist.js
+++ assets/js/wishlist.js
@@ -1,19 +1,39 @@
 (function ($) {
   "use strict";
 
-  let wishList = {};
+  var wishList = {};
 
-  const setWishlist = (prod) => {
-    wishList = {...wishList, ...prod};
-    updateWishlist();
+  var setWishlist = function(prod){
+    wishList = Object.assign(wishList, prod);
+    updateWishlistInCookie();
+
+    var cart = getCartFromSessionStorage();
+    if (ifProductExistInCart(Object.keys(prod)[0])){
+      cart = Object.assign(cart, prod);
+      saveCartToSessionStorage(cart)
+    }
+
   };
 
-  const updateWishlist = () => {
+  var updateWishlistInCookie = function(){
     Cookies.set("wishlist", JSON.stringify(wishList));
+    reRenderHeaderMiniCart()
   };
 
-  // This function called when user open
-  const initWishlist = () => {
+  var reRenderHeaderMiniCart = function(){
+    var numberOfProducts = 0;
+    var products = getCartFromSessionStorage();
+    var totalPrice = 0;
+
+    for(var key in products){
+      numberOfProducts += products[key]['amount'];
+      totalPrice += products[key]['amount'] * products[key]['price']
+    }
+    $(".header-mini-cart-amount").html(numberOfProducts + ' ($' + totalPrice + ')');
+  };
+
+  // This function called when start
+  var initWishlist = function(){
     loadWishList();
 
     $(".to-wishlist-btn").on("click", addProductToWishList);
@@ -23,54 +43,77 @@
   };
 
   // Function for loading selected products from sessionStorage
-  const loadWishList = () => {
+  var loadWishList = function(){
     if (Cookies.get("wishlist")) {
       wishList = JSON.parse(Cookies.get("wishlist"));
     }
 
-    for (let key in wishList) {
-      let selector = `[data-id=${key.toString()}]`;
+    for (var key in wishList) {
+      var selector = '[data-id=' + key.toString() + ']';
       $(selector).find(".to-wishlist-btn").html("Добавлено");
     }
   };
 
   // Add or delete products in cart
   const addProductToWishList = (event) => {
-    let element = event.target;
-    let productElement = $(element).closest(".product-item");
+    var element = event.target;
+    var productElement = $(element).closest(".product-item");
     productElement = $(productElement);
 
-    let dataId = productElement.attr("data-id");
-    let img = productElement.find(".image img").attr("src");
-    let name = productElement.find(".title a").html();
-    let price = productElement.find(".inner-price").html();
+    var dataId = productElement.attr("data-id");
+    var img = productElement.find(".image img").attr("src");
+    var name = productElement.find(".title a").html();
+    var price = productElement.find(".inner-price").html();
 
-    let prod = { [dataId]: { name, img, price, amount: 1 } };
+    var prod = { [dataId]: { name, img, price, amount: 1 } };
+
+    var btn = productElement.find(".to-wishlist-btn");
 
     if (!wishList[dataId]) {
-      wishList = { ...prod, ...wishList };
-      productElement.find(".to-wishlist-btn").html("Добавлено");
+      wishList = Object.assign(wishList, prod);
+      //если кнопка имеет класс single-wishlist-btn, просто меняем фон
+      btn.hasClass('single-wishlist-btn')? btn.css({"background-color":'#63D1B5'}) : btn.html("Добавлено");
     } else {
       delete wishList[dataId];
-      productElement.find(".to-wishlist-btn").html("В избранное");
+      //если кнопка имеет класс single-wishlist-btn, просто меняем фон
+      btn.hasClass('single-wishlist-btn')? btn.css({"background-color":'#b663d1'}) : btn.html("В избранное") ;
     }
 
     Cookies.set("wishlist", JSON.stringify(wishList));
     renderHeaderMiniWishList();
   };
 
-  const renderHeaderMiniWishList = () => {
-    let numberOfProducts = Object.keys(wishList).length;
+  var renderHeaderMiniWishList = function(){
+    var numberOfProducts = Object.keys(wishList).length;
     $(".wishlist-amount").html(numberOfProducts);
   };
 
   // WISHLIST PAGE
   // Function for rendering products to wishList.html
-  const renderWishList = () => {
-    let out = "";
 
-    for (let key in wishList) {
-      out += `<tr class='product-item' data-id='${key}'>`;
+  var ifProductExistInCart = function (id) {
+    var cart = getCartFromSessionStorage();
+    return  cart.hasOwnProperty(id)
+  };
+
+  var getCartFromSessionStorage = function () {
+    var cart = JSON.parse(sessionStorage.getItem('cart'));
+    return cart ? cart : {}
+  };
+
+  var saveCartToSessionStorage = function(cart) {
+    sessionStorage.setItem("cart", JSON.stringify(cart));
+    reRenderHeaderMiniCart();
+  };
+
+  var renderWishList = function(){
+    var out = "";
+
+    for (var key in wishList) {
+
+      var productStatus = ifProductExistInCart(key) ? 'Добавлено' : 'В корзину';
+
+      out += '<tr class="wishlist-product" data-id="' + key + '">';
       out +=
         '<td class="pro-thumbnail image"><a href="#"><img src="' +
         wishList[key].img +
@@ -79,12 +122,12 @@
         '<td class="pro-title title"><a href="#">' +
         wishList[key].name +
         "</a></td>";
-      out += `<td class="pro-price"><span class="amount">$ <span class="inner-price">${wishList[key].price}</span> `;
+      out += '<td class="pro-price"><span class="amount">$<span class="inner-price">' + wishList[key].price + '</span>';
       out +=
         '<td class="pro-quantity"><div class="pro-qty"><input type="text" class="pro-qty-input" value="' +
         wishList[key].amount +
         '"></div></td>';
-      out += `<td class="pro-add-cart"><a class="to-cart-btn">В корзину</a></td>`;
+      out += '<td class="pro-add-cart"><a class="to-cart-from-wishlist-btn">' + productStatus + '</a></td>';
       out += '<td class="pro-remove"><a>×</a></td>';
       out += "</tr>";
     }
@@ -92,10 +135,11 @@
     $(".wishlist-container").html(out);
 
     onQuantityChangeListeners();
-    onDeleteListener()
+    onDeleteListener();
+    onAddToCartListener();
   };
 
-  const onQuantityChangeListeners = () => {
+  var onQuantityChangeListeners = function(){
     /*-----
       Quantity
   --------------------------------*/
@@ -105,11 +149,11 @@
     // Сработает когда пользователь изменяет количество
     $('.qtybtn').on('click', function()  {
 
-      let $button = $(this);
-      let dataId = $button.closest('.product-item').attr('data-id');
+      var $button = $(this);
+      var dataId = $button.closest('.wishlist-product').attr('data-id');
 
-      let oldValue = $button.parent().find('input').val();
-      let newVal = 1;
+      var oldValue = $button.parent().find('input').val();
+      var newVal = 1;
       if ($button.hasClass('inc')) {
         newVal = parseFloat(oldValue) + 1;
       } else {
@@ -117,7 +161,7 @@
         newVal = oldValue > 1 ? (parseFloat(oldValue) - 1) : 1
       }
 
-      let prod = { [dataId]: { name: wishList[dataId].name, img: wishList[dataId].img ,price: wishList[dataId].price , amount: newVal }};
+      var prod = { [dataId]: { name: wishList[dataId].name, img: wishList[dataId].img ,price: wishList[dataId].price , amount: newVal }};
 
       $button.parent().find('input').val(newVal);
       setWishlist(prod);
@@ -126,32 +170,53 @@
     //
     $('.pro-qty input').on('input', function () {
 
-      let $input = $(this);
-      let dataId = $input.closest('.cart-product').attr('data-id');
+      var $input = $(this);
+      var dataId = $input.closest('.cart-product').attr('data-id');
 
-      let newVal = parseFloat($input.val());
+      var newVal = parseFloat($input.val());
       if (isNaN(newVal)){
         newVal = 0;
       }
 
-      let prod = { [dataId]: { name: wishList[dataId].name, img: wishList[dataId].img ,price: wishList[dataId].price , amount: newVal }};
+      var prod = { [dataId]: { name: wishList[dataId].name, img: wishList[dataId].img ,price: wishList[dataId].price , amount: newVal }};
 
       setWishlist(prod);
     })
   };
 
-  const onDeleteListener = () => {
-    $(".pro-remove a").on("click", (e) => {
-      //Удаление из избранных при нажатии на крест в таблице
-      //отменяем дефолтное поведение кнопки
+  var onAddToCartListener = function () {
+    $('.to-cart-from-wishlist-btn').on('click', function (event) {
+      var $button = $(event.target);
+      var productId = $button.closest('.wishlist-product').attr('data-id');
+
+      var cart = getCartFromSessionStorage();
+      var product = {[productId]: wishList[productId]};
+
+      var selector = '[data-id=' + productId.toString() + ']';
+
+      if (cart.hasOwnProperty(productId)){
+        delete cart[productId];
+        $(selector).find(".to-cart-from-wishlist-btn").html("В корзину");
+      } else {
+        cart = Object.assign(cart, product);
+        $(selector).find(".to-cart-from-wishlist-btn").html("Добавлено");
+      }
+
+      saveCartToSessionStorage(cart);
+    })
+  };
+
+  var onDeleteListener = function(){
+    $('.pro-remove a').on('click', function (e) {
       e.preventDefault();
-      //функция удаляет обьект из избранных, если она уже в избранных
-      addProductToWishList(e);
-      //Находим родительскую строку
-      let parent = e.target.closest(".product-item");
-      //удаляем ее из таблицы
-      parent.parentNode.removeChild(parent).fadeOut();
-    });
+
+      var dataId = $(this).closest('.wishlist-product').attr('data-id');
+      if (dataId){
+        delete wishList[dataId];
+        updateWishlistInCookie();
+        renderWishList();
+      }
+    })
   };
 
   initWishlist();
